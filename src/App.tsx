334bd3ffa4431d9b7654d66d7a09e5b6965525bc
AndroPyro28 <menandroeugenio1028@gmail.com>
2023-09-11 03:19:13 +0800

    lights

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
 import { Canvas } from "@react-three/fiber";
 import AnimatedBox from "./components/AnimatedBox";
 import { OrbitControls, Stats, useTexture } from "@react-three/drei";
+import Lights from "./components/Lights";
+import Ground from "./components/Ground";
 
 function App() {
-
   const TextureSpheres = () => {
     const map = useTexture("../public/textures/metal_plate_diff_1k.png");
-    const displacementMap = useTexture("../public/textures/metal_plate_disp_1k.png");
-    const normalMap = useTexture("../public/textures/metal_plate_nor_gl_1k.png");
-    const roughnessMap = useTexture("../public/textures/metal_plate_rough_1k.png");
+    // const displacementMap = useTexture("../public/textures/metal_plate_disp_1k.png");
+    const normalMap = useTexture(
+      "../public/textures/metal_plate_nor_gl_1k.png"
+    );
+    const roughnessMap = useTexture(
+      "../public/textures/metal_plate_rough_1k.png"
+    );
 
     return (
       <>
-        <mesh scale={[0.5, 0.5, 0.5]} position={[-1, 0, 0]}>
+        <mesh scale={[0.5, 0.5, 0.5]} position={[0, 1, 0]} castShadow>
           <sphereGeometry />
-          <meshStandardMaterial map={map} />
-        </mesh>
-
-        <mesh scale={[0.5, 0.5, 0.5]} position={[0, 0, 0]}>
-          <sphereGeometry />
-          <meshStandardMaterial map={map} normalMap={normalMap} roughnessMap={roughnessMap} />
-        </mesh>
-
-        <mesh scale={[0.5, 0.5, 0.5]} position={[1, 0, 0]}>
-          <sphereGeometry args={[1,200,200]}/>
-          <meshStandardMaterial map={map} normalMap={normalMap} roughnessMap={roughnessMap} displacementMap={displacementMap} displacementScale={0.05} />
+          <meshStandardMaterial
+            map={map}
+            normalMap={normalMap}
+            roughnessMap={roughnessMap}
+          />
         </mesh>
       </>
     );
@@ -33,15 +32,14 @@ function App() {
   const testDevelopmentMode = true;
   return (
     <div className="container">
-      <Canvas className="canvas">
-        {/* <PerspectiveCamera makeDefault position={[0,5,10]}/> */}
+      <Canvas className="canvas" shadows>
+        {/* <PerspectiveCamera makeDefault position={[0,3,10]}/> */}
         {testDevelopmentMode && <Stats />}
         {testDevelopmentMode && <axesHelper args={[2]} />}
         {testDevelopmentMode && <gridHelper args={[10, 10]} />}
         <OrbitControls />
-        <ambientLight intensity={0.3} />
-        <directionalLight color={"white"} position={[0, 5, 5]} />
-        {/* <AnimatedBox testMode={testDevelopmentMode} /> */}
+        <Lights />
+        <Ground />
         <TextureSpheres />
       </Canvas>
     </div>
